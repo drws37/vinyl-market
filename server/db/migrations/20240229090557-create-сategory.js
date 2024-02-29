84d173cf79c9a3676149c84c6b8bacb57be9c3f0
Ivan Kabatskii <ivankabatsky@gmail.com>
2024-02-29 17:23:29 +0300

    feat: add seeders

--- server/db/migrations/20240229090557-create-сategory.js
+++ server/db/migrations/20240229090557-create-сategory.js
@@ -2,28 +2,28 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Сategories', {
+    await queryInterface.createTable('Categories', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       title: {
         allowNull: false,
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Сategories');
-  }
+    await queryInterface.dropTable('Categories');
+  },
 };
