46967273b0e4361ac949123f620e3e3300e99846
Ivan Kabatskii <ivankabatsky@gmail.com>
2024-03-01 13:28:12 +0300

    feat: add profile page

--- client/src/features/Profile/ProfilePage.tsx
+++ client/src/features/Profile/ProfilePage.tsx
@@ -1,14 +1,44 @@
-import React from 'react';
+/* eslint-disable no-nested-ternary */
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import type { RootState } from '../../store/store';
+import './styles/profile.css';
 
 function ProfilePage(): JSX.Element {
   const user = useSelector((store: RootState) => store.auth.user);
 
+  const [content, setContent] = useState('personalData');
+
   return (
     <>
-      <div>ProfilePage</div>
-      <div>{user?.username}</div>
+      <h1>ProfilePage</h1>
+      <div className="profile_main">
+        <div className="sidebar">
+          <button type="button" onClick={() => setContent('personalData')}>
+            Личные данные
+          </button>
+          <button type="button" onClick={() => setContent('cart')}>
+            Корзина
+          </button>
+          <button type="button" onClick={() => setContent('orders')}>
+            Мои заказы
+          </button>
+          <button type="button" onClick={() => setContent('favorites')}>
+            Избранное
+          </button>
+        </div>
+        <div>
+          {content === 'personalData' ? (
+            <div>PERSONAL DATA</div>
+          ) : content === 'cart' ? (
+            <div>КОРЗИНА</div>
+          ) : content === 'orders' ? (
+            <div>ЗАКАЗЫ</div>
+          ) : (
+            <div>ИЗБРАННОЕ</div>
+          )}
+        </div>
+      </div>
     </>
   );
 }
