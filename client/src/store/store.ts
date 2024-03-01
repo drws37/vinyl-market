a0b6bc9f6148ff90ac961b9eaa0156ad54221cf9
Ivan Kabatskii <150283306+drws37@users.noreply.github.com>
2024-03-01 09:49:42 +0300

    Update store.ts

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -2,17 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
 import recordsSlice from '../features/Catalog/recordsSlice'
 
-
-
-
 export const store = configureStore({
  reducer: {
   records: recordsSlice
  },
 });
 
-
-export type RootState = ReturnType<typeof store.getState>;
-
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch: () => AppDispatch = useDispatch;
+
+export type RootState = ReturnType<typeof store.getState>;
