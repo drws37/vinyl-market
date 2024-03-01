cf4caedccb8bb2f98208bc162fa78513497f32a0
Nikita <nik.aksenovv@gmail.com>
2024-03-01 18:19:21 +0300

    feat:searchSysteam 2/0v

--- client/src/features/Catalog/components/FilteredList.tsx
+++ client/src/features/Catalog/components/FilteredList.tsx
@@ -6,10 +6,12 @@ function FilteredList({record}):JSX.Element {
 
 
   return (
+    <Link  className='div-link-search' to={`/records/${record.id}` }>
 <div className='input-search' >
     <img className='img-record' src={record.img} alt="" />
-    <Link className='link-search'>{record.title}</Link>
+    <Link  className='link-search'>{record.title}</Link>
 </div>
+</Link>
 
 
 
