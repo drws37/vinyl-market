d6e2dde5d836940f839c5c25560c2a493b3c7e33
Mark Kim <mark@Mark-MacBook-Pro.local>
2024-03-02 16:42:37 +0300

    feat: client update form

--- client/src/features/Catalog/components/RecordPage.tsx
+++ client/src/features/Catalog/components/RecordPage.tsx
@@ -1,17 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router';
-import type { RootState } from '../../../store/store';
+import { useAppDispatch, type RootState } from '../../../store/store';
+import { recordUpdate } from '../recordsSlice';
+import type { Record } from '../type';
+
+const { recordId } = useParams();
+const records = useSelector((store: RootState) => store.records.records);
+const currentRecord = recordId ? records.find((record) => record.id === +recordId) : undefined;
+
+export const fetchRecordUpdate = async (obj: FormData): Promise<Record> => {
+  const res = await fetch(`api/records/${currentRecord?.id}`, {
+    method: 'put',
+    body: obj,
+  })
+  const data = await res.json()
+  return data.record
+}
 
 function RecordPage(): JSX.Element {
-  const { recordId } = useParams();
-  const records = useSelector((store: RootState) => store.records.records);
-  const currentRecord = recordId ? records.find((record) => record.id === +recordId) : undefined;
+
+  const [title, setTitle] = useState('')
+  const [artist, setArtist] = useState('')
+  const [description, setDescription] = useState('')
+  const [img, setImg] = useState<FileList | null | undefined>(null)
+  const [price, setPrice] = useState('')
+  const [quality, setQuality] = useState('')
+
+  const dispatch = useAppDispatch()
+  
+
+  const updateRecordFetch = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault()
+    const imgFile = img?.[0]
+    const formData = new FormData()
+    formData.append('title', title)
+    formData.append('artist', artist)
+    formData.append('description', description)
+    formData.append('price', price)
+    formData.append('img', imgFile !== null && imgFile !== undefined ? imgFile : '')
+    formData.append('quality', quality)
+    
+    dispatch(recordUpdate(formData)).catch(console.log)
+  }
+
 
   return (
     <div>
       {currentRecord && (
         <>
+          <div className='update__form__container'>
+      <form className='update__form' onSubmit={updateRecordFetch}>
+            <input defaultValue={currentRecord?.title} value={title} placeholder='title' required onChange={(e)=>setTitle(e.target.value)} />
+            <input defaultValue={currentRecord?.artist} value={artist} placeholder='artist' onChange={(e)=>setArtist(e.target.value)}/>
+            <input defaultValue={currentRecord?.description} value={description} placeholder='description' onChange={(e)=>setDescription(e.target.value)}/>
+            <input defaultValue={currentRecord?.price} value={price} placeholder='price' onChange={(e)=>setPrice(e.target.value)}/>
+            <input  placeholder='img' type='file' onChange={(e)=>setImg(e.target.files)}/>
+            <select defaultValue={currentRecord?.quality} value={quality} onChange={(e) => setQuality(e.target.value)}>
+              <option value="empty">Не выбрано</option>
+              <option value="mint">Mint</option>
+              <option value="near-mint">Near mint</option>
+              <option value="very-good">Very good</option>
+              <option value="good">Good</option>
+              <option value="fair">Fair</option>
+              <option value="poor">Poor</option>
+              <option value="bad">Bad</option>
+            </select>
+            <button type='submit'>Добавить</button>
+        </form>
+    </div>
           <div className="record-page_main">
             <div className="record-card_main">
               <div className="record-card_img">
--- client/src/features/Catalog/recordsSlice.ts
+++ client/src/features/Catalog/recordsSlice.ts
@@ -1,6 +1,8 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import * as api from './api';
 import type { StateRecords } from './type';
+// eslint-disable-next-line import/no-cycle
+import { fetchRecordUpdate } from './components/RecordPage';
 
 const initialState: StateRecords = {
   records: [],
@@ -11,6 +13,9 @@ export const recordsLoad = createAsyncThunk('records/load', () => api.fetchReocr
 export const recordAdd = createAsyncThunk('records/add', (obj: FormData) =>
   api.fetchRecordAdd(obj),
 );
+export const recordUpdate = createAsyncThunk('records/update', (obj: FormData) =>
+  fetchRecordUpdate(obj),
+)
 
 const recordsSlice = createSlice({
   name: 'records',
@@ -21,7 +26,9 @@ const recordsSlice = createSlice({
     addCase(recordsLoad.fulfilled, (state, action) => {state.records = action.payload}).
     addCase(recordsLoad.rejected, (state, action) => {state.message = action.error.message}).
     addCase(recordAdd.fulfilled, (state, action) => {state.records.unshift(action.payload)}).
-    addCase(recordAdd.rejected, (state, action) => {state.message = action.error.message})
+    addCase(recordAdd.rejected, (state, action) => {state.message = action.error.message}).
+    addCase(recordUpdate.fulfilled, (state, action) => {state.records.map((record) => record.id === action.payload.id ? action.payload : record)}).
+    addCase(recordUpdate.rejected, (state, action) => {state.message = action.error.message})
   }
 })
 
