8966e49feb010d83439e5ffacfc7f2934711a947
Ivan Kabatskii <ivankabatsky@gmail.com>
2024-03-04 09:17:37 +0300

    fix: TS error in orderDelete

--- client/src/features/Catalog/api.ts
+++ client/src/features/Catalog/api.ts
@@ -1,74 +1,76 @@
 /* eslint-disable @typescript-eslint/no-base-to-string */
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable import/no-duplicates */
-import type { Category, OrderItem, OrderItemId, Record } from "./type";
+import type { Category, OrderItem, OrderItemId, Record } from './type';
 // eslint-disable-next-line import/no-duplicates
-import type { RecordId } from "./type";
+import type { RecordId } from './type';
 
 export const fetchReocrdsLoad = async (): Promise<Record[]> => {
-  const res = await fetch('/api/records')
-  const data = await res.json()
-  return data.records
-}
+  const res = await fetch('/api/records');
+  const data = await res.json();
+  return data.records;
+};
 
-export const fetchRecordAdd = async (obj:FormData): Promise<Record> => {
+export const fetchRecordAdd = async (obj: FormData): Promise<Record> => {
   const res = await fetch('/api/records', {
     method: 'post',
     body: obj,
-  })
-  const data = await res.json()
-  return data.record
-}
+  });
+  const data = await res.json();
+  return data.record;
+};
 
 export const fetchCategoriesLoad = async (): Promise<Category[]> => {
-  const res = await fetch('/api/categories')
-  const data = await res.json()
-  return data.categories
-}
+  const res = await fetch('/api/categories');
+  const data = await res.json();
+  return data.categories;
+};
 
-
-export const fetchRecordUpdate = async (obj:{id: RecordId | undefined, obj: FormData}): Promise<Record> => {
+export const fetchRecordUpdate = async (obj: {
+  id: RecordId | undefined;
+  obj: FormData;
+}): Promise<Record> => {
   const res = await fetch(`/api/records/${obj.id}`, {
     method: 'put',
     body: obj.obj,
-  })
-  const data = await res.json()
-  return data
-}
+  });
+  const data = await res.json();
+  return data;
+};
 
-export const fetchOrderAdd =async (obj:{id:number, status:string}):Promise<OrderItem[]> => {
+export const fetchOrderAdd = async (obj: { id: number; status: string }): Promise<OrderItem[]> => {
   console.log(obj, '1111');
-  
-  const res = await fetch (`/api/order`, {
+
+  const res = await fetch(`/api/order`, {
     method: 'POST',
-    headers:{'Content-Type' : 'application/json'},
-    body:JSON.stringify(obj)
-  })
-  const data = await res.json()
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(obj),
+  });
+  const data = await res.json();
   // console.log(data);
-  
-  return data
-  
-}
+
+  return data;
+};
 
 export const fetchOrdersLoad = async (): Promise<OrderItem[]> => {
-  const res = await fetch('/api/order/order')
-  const data = await res.json()
+  const res = await fetch('/api/order/order');
+  const data = await res.json();
   // console.log(data);
-  
-  return data
-}
 
-export const fetchOrderDel = async (id:OrderItemId): Promise<OrderItemId> => {
-  const res = await fetch(`/api/order/${id}`, {method: 'DELETE'})
-  const data = await res.json()
+  return data;
+};
 
+export const fetchOrderDel = async (id: OrderItemId): Promise<OrderItemId> => {
+  const res = await fetch(`/api/order/${id}`, { method: 'DELETE' });
+  const data = await res.json();
+  return data;
+};
 export const fetchRecordDelete = async (id: RecordId | undefined): Promise<RecordId> => {
   const res = await fetch(`/api/records/${id}`, {
-    method: 'delete'
-  })
-  const data = await res.json()
+    method: 'delete',
+  });
+  const data = await res.json();
   console.log(data);
 
-  return data
-}
+  return data;
+};
