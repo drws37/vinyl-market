40101d129245588ce60bb27da926dfd6e0588975
Ivan Kabatskii <ivankabatsky@gmail.com>
2024-03-04 09:31:36 +0300

    feat: add auth navigation to main page

--- client/src/features/Auth/components/Registration.tsx
+++ client/src/features/Auth/components/Registration.tsx
@@ -1,13 +1,12 @@
-import React from 'react'
+import React from 'react';
 import type { SubmitHandler } from 'react-hook-form';
 import { useForm } from 'react-hook-form';
 import { object, ref, string } from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
-import { NavLink } from 'react-router-dom'
-import '../styles/form.css'
+import { NavLink, useNavigate } from 'react-router-dom';
+import '../styles/form.css';
 import { useSelector } from 'react-redux';
-import type { RootState } from '@reduxjs/toolkit/query';
-import store, { useAppDispatch } from '../../../store/store';
+import { type RootState, useAppDispatch } from '../../../store/store';
 import type { User } from '../type';
 import { authRegistration } from '../authSlice';
 
@@ -23,81 +22,90 @@ const checkfild = object().shape({
     .min(8, 'Пароль должен быть более 8 символов')
     .max(25, 'Пароль должен быть не более 25 символов')
     .oneOf([ref('password')], 'Пароли не совпадают'),
-    role: string().required('Необходимо указать роль'),
+  role: string().required('Необходимо указать роль'),
 });
 
+function Registration(): JSX.Element {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const message = useSelector((store: RootState) => store.auth.message);
 
-function Registration():JSX.Element {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<User>({ resolver: yupResolver(checkfild) });
 
-const dispatch = useAppDispatch();
-const message = useSelector((store:RootState) => store.auth.message)
+  const registration: SubmitHandler<User> = (data: User) => {
+    dispatch(authRegistration(data)).catch(console.log);
+    navigate('/');
+  };
 
-
-
-
-
-const {
-  register,
-  handleSubmit,
-  formState:{errors},
-
-} = useForm<User>({resolver: yupResolver(checkfild)})
-
-const registration :SubmitHandler<User> = (data:User) => {
-dispatch(authRegistration(data)).catch(console.log)
-}
-
-  
   return (
     <div className="container">
- <div className="row">
-
- <div className="col-md-offset-3 col-md-6">
- <form className="form-horizontal" onSubmit={handleSubmit(registration)}>
- <span className="heading">Регистрация</span>
- <div className="form-group">
- <input type="text" className="form-control" placeholder="Username" {...register('username')}/>
- <i className="fa fa-user" />
- <span>{errors.username?.message}</span>
- </div>
- <div className="form-group">
- <input type="email" className="form-control" placeholder="E-mail" {...register('email')}/>
- <i className="fa fa-user" />
- <span>{errors.email?.message}</span>
- </div>
- <div className="form-group">
- <input type="password" className="form-control" placeholder="Password" {...register('password')}/>
- <i className="fa fa-user" />
- <span>{errors.password?.message}</span>
-
- </div>
- <div className="form-group">
- <input type="password" className="form-control"  placeholder="Repeat password" {...register('rpassword')}/>
- <i className="fa fa-user" />
- <span>{errors.rpassword?.message}</span>
-
- </div>
- <div className="form-group help">
-<select {...register('role')} > 
-<option value="buyer" >Покупатель</option>
-<option value="seller" >Магазин</option>
-<span>{errors.role?.message}</span>
-
-
-</select>
- <NavLink to='#' className="fa fa-question-circle" />
- </div>
- <div className="form-group">
- <button type="submit" className="btn btn-default">ВХОД</button>
- </div>
- </form>
- <div className='errRega err'> {message}</div>
- </div>
-
- </div>
-</div>
-
-  )
+      <div className="row">
+        <div className="col-md-offset-3 col-md-6">
+          <form className="form-horizontal" onSubmit={handleSubmit(registration)}>
+            <span className="heading">Регистрация</span>
+            <div className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Username"
+                {...register('username')}
+              />
+              <i className="fa fa-user" />
+              <span>{errors.username?.message}</span>
+            </div>
+            <div className="form-group">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="E-mail"
+                {...register('email')}
+              />
+              <i className="fa fa-user" />
+              <span>{errors.email?.message}</span>
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Password"
+                {...register('password')}
+              />
+              <i className="fa fa-user" />
+              <span>{errors.password?.message}</span>
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Repeat password"
+                {...register('rpassword')}
+              />
+              <i className="fa fa-user" />
+              <span>{errors.rpassword?.message}</span>
+            </div>
+            <div className="form-group help">
+              <select {...register('role')}>
+                <option value="buyer">Покупатель</option>
+                <option value="seller">Магазин</option>
+                <span>{errors.role?.message}</span>
+              </select>
+              <NavLink to="#" className="fa fa-question-circle" />
+            </div>
+            <div className="form-group">
+              <button type="submit" className="btn btn-default">
+                ВХОД
+              </button>
+            </div>
+          </form>
+          <div className="errRega err"> {message}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Registration
+export default Registration;
