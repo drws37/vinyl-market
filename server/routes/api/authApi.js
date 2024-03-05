f1547537cbb87468a8530155b0fc95080c340eb4
Ivan Kabatskii <ivankabatsky@gmail.com>
2024-03-05 12:47:15 +0300

    fix: fixed reg for new db

--- server/routes/api/authApi.js
+++ server/routes/api/authApi.js
@@ -10,35 +10,46 @@ const router = express.Router();
 router.post('/registration', async (req, res) => {
   try {
     const { username, email, password, role } = req.body;
-    // console.log(req.body);
 
     if (username && email && password && role) {
-      const globalRegex = /^[_a-z0-9-\+-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/i;
+      const globalRegex =
+        /^[_a-z0-9-\+-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/i;
 
       if (globalRegex.test(email)) {
         let user = await User.findOne({ where: { email } });
         if (user) {
-          res.status(400).json({ message: 'Такой пользователь уже существует' });
+          res
+            .status(400)
+            .json({ message: 'Такой пользователь уже существует' });
         } else {
           const hash = await bcrypt.hash(password, 10);
-          user = await User.create({ username, email, password: hash, role });
-          const { accessToken, refreshToken } = generateTokens({ user: { username: user.username, id: user.id, role: user.role } });
-          // console.log('123');
-          res.cookie(
-            cookiesConfig.access,
-            accessToken,
-            { maxAge: cookiesConfig.maxAgeAccess, httpOnly: true },
-          );
-          res.cookie(
-            cookiesConfig.refresh,
-            refreshToken,
-            { maxAge: cookiesConfig.maxAgeRefresh, httpOnly: true },
-          );
-          // console.log(123);
-          res.status(201).json({ message: 'success', user: { username: user.name, id: user.id } });
+          user = await User.create({
+            username,
+            email,
+            password: hash,
+            role,
+            img: '/profileImg/user.png',
+          });
+          const { accessToken, refreshToken } = generateTokens({
+            user: { username: user.username, id: user.id, role: user.role },
+          });
+          res.cookie(cookiesConfig.access, accessToken, {
+            maxAge: cookiesConfig.maxAgeAccess,
+            httpOnly: true,
+          });
+          res.cookie(cookiesConfig.refresh, refreshToken, {
+            maxAge: cookiesConfig.maxAgeRefresh,
+            httpOnly: true,
+          });
+          res.status(201).json({
+            message: 'success',
+            user: { username: user.name, id: user.id },
+          });
         }
       } else {
-        res.status(400).json({ message: 'Ваша почта не соответствует формату' });
+        res
+          .status(400)
+          .json({ message: 'Ваша почта не соответствует формату' });
       }
     } else {
       res.status(400).json({ message: 'Заполните все поля' });
@@ -54,21 +65,21 @@ router.post('/login', async (req, res) => {
     if (email && password) {
       const user = await User.findOne({ where: { email } });
       if (user && (await bcrypt.compare(password, user.password))) {
-        const { accessToken, refreshToken } = generateTokens(
-          { user: { name: user.name, id: user.id, role: user.role } },
-        );
+        const { accessToken, refreshToken } = generateTokens({
+          user: { name: user.name, id: user.id, role: user.role },
+        });
 
-        res.cookie(
-          cookiesConfig.access,
-          accessToken,
-          { maxAge: cookiesConfig.maxAgeAccess, httpOnly: true },
-        );
-        res.cookie(
-          cookiesConfig.refresh,
-          refreshToken,
-          { maxAge: cookiesConfig.maxAgeRefresh, httpOnly: true },
-        );
-        res.status(200).json({ message: 'success', user: { name: user.name, id: user.id } });
+        res.cookie(cookiesConfig.access, accessToken, {
+          maxAge: cookiesConfig.maxAgeAccess,
+          httpOnly: true,
+        });
+        res.cookie(cookiesConfig.refresh, refreshToken, {
+          maxAge: cookiesConfig.maxAgeRefresh,
+          httpOnly: true,
+        });
+        res
+          .status(200)
+          .json({ message: 'success', user: { name: user.name, id: user.id } });
       } else {
         res.status(400).json({ message: 'логин или пароль не верный' });
       }
@@ -83,7 +94,10 @@ router.post('/login', async (req, res) => {
 router.get('/check', async (req, res) => {
   // console.log(res.locals.user, 'res.locals.user');
   if (res.locals.user) {
-    const user = await User.findOne({ where: { id: res.locals.user.id }, attributes: { exclude: ['password'] } });
+    const user = await User.findOne({
+      where: { id: res.locals.user.id },
+      attributes: { exclude: ['password'] },
+    });
     res.json({ user });
     return;
   }
