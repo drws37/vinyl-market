96de16cabff9c7ca92c4ab5fbeb1559c0b257d1b
Ivan Kabatskii <ivankabatsky@gmail.com>
2024-03-06 17:52:49 +0300

    feat: footer

--- client/src/features/Main/Footer.tsx
+++ client/src/features/Main/Footer.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import './styles/footer.scss';
 
-function Footer():JSX.Element {
+function Footer(): JSX.Element {
   return (
-    <div className='footer'>Footer</div>
-  )
+    <div className="footer">
+      <div className="footer_top">
+        <p>ВИНИЛ</p>
+        <img src="/public/lp.png" alt="vinyl record" />
+        <p>МАРКЕТ</p>
+      </div>
+      <div className="footer_bottom">
+        <p>2024</p>
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
